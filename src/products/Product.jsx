3dc9d2f95cce716700dbb024b4dbbe38afa61d8f
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-02-03 11:10:37 +0300

    changes

--- src/products/Product.jsx
+++ src/products/Product.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
+export default function Product({ user }) {
+  const { productId } = useParams();
+  const [product, setProduct] = useState(null);
 
-export default function Product({user}){
- const {productId} = useParams()
- const [product, setProduct] = useState(null)
-
- function handleClick(product) {
+  function handleClick(product) {
     let localFavs = JSON.parse(localStorage.getItem("favs")) ?? [];
     const itemIndex = localFavs.findIndex(
       (localItem) => product.id === localItem.id
@@ -20,37 +21,39 @@ export default function Product({user}){
     localStorage.setItem("favs", JSON.stringify(localFavs));
   }
 
-
-  useEffect(()=>{
+  useEffect(() => {
     fetch(`https://fakestoreapi.com/products/${productId}`)
-            .then(res=>res.json())
-            .then(json=>setProduct(json))
-            .catch(err=> console.error(err))
-   }, [productId])
-   if(!product) return <></>
-    return (
-        <>
-        <div className="row row-cols-3">
-            <div className="col shadow rounded-3 p-2">
+      .then((res) => res.json())
+      .then((json) => setProduct(json))
+      .catch((err) => console.error(err));
+  }, [productId]);
+  if (!product) return <></>;
+  return (
+    <>
+      <div className="row row-cols-2">
+        <div className="col shadow rounded-3 p-2">
           <div className="card h-100">
-            
-            <img src={product.image} className="w-100 img-thumbnail" alt="..." />
-           
+            <img
+              src={product.image}
+              className="w-100 img-thumbnail"
+              alt="..."
+            />
+
             <div className="card-body">
               <h5 className="card-title">{product.title}</h5>
               <p className="card-text">{product.description}</p>
               <h6>${product.price}</h6>
-               <a
+              <a
                 href="#"
                 className="btn btn-warning"
                 onClick={() => handleClick(product)}
-              >Fav
+              >
+                Fav
               </a>
-            </div></div>
+            </div>
           </div>
         </div>
-            
-         
-        </>
-    )
+      </div>
+    </>
+  );
 }
